06299ce60b565953660ac498093c7bd6d8894c82
Walid AlDari <walidaldari1@gmail.com>
2024-10-16 13:06:23 -0400

    Added recurring starts in response to scraping errors

--- DataPipeline/Scraper/helper.py
+++ DataPipeline/Scraper/helper.py
@@ -3,5 +3,10 @@ import json
 
 def add_json_entry(entry, file_path):
     with open(file_path, 'a') as file:
-        json.dump(JSON(entry), file)
+        json.dump(entry, file)
         file.write('\n')
+
+def count_lines_in_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        return len(lines)
--- DataPipeline/Scraper/main.py
+++ DataPipeline/Scraper/main.py
@@ -3,73 +3,88 @@ from selenium.webdriver.common.by import By
 from time import sleep
 import json
 import random
-from helper import add_json_entry
+from helper import *
+import traceback
 
-driver = webdriver.Chrome()
-driver.get('https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm')
-driver.implicitly_wait(0.5)
+courseRowValue = None
 
-# Navigating to "Course Campus" tab on the left, and proceeding to select "Keele" to load up all courses offered at keele.
-driver.find_element(By.XPATH, "/html/body/p/table/tbody/tr[2]/td[1]/table/tbody/tr[6]/td/a/img").click()
-driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/form/table/tbody/tr[2]/td[2]/select/option[3]").click()
-driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/form/table/tbody/tr[3]/td[2]/input").click()
+processing = True 
 
-driver.implicitly_wait(5)
+while processing: 
 
+    try: 
+        driver = webdriver.Chrome()
+        driver.get('https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm')
+        driver.implicitly_wait(0.5)
 
-# Creating a new tab to switch to
-tab_main_id = driver.current_window_handle
-driver.switch_to.new_window('tab')
-tab_secondary_id = driver.current_window_handle
-driver.switch_to.window(tab_main_id)
+        # Navigating to "Course Campus" tab on the left, and proceeding to select "Keele" to load up all courses offered at keele.
+        driver.find_element(By.XPATH, "/html/body/p/table/tbody/tr[2]/td[1]/table/tbody/tr[6]/td/a/img").click()
+        driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/form/table/tbody/tr[2]/td[2]/select/option[3]").click()
+        driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/form/table/tbody/tr[3]/td[2]/input").click()
 
-#Iterate through course links
-courseRowValue = 2
+        driver.implicitly_wait(5)
 
-# Save course details within a txt file
-courseItinerary = []
+        # Creating a new tab to switch to
+        tab_main_id = driver.current_window_handle
+        driver.switch_to.new_window('tab')
+        tab_secondary_id = driver.current_window_handle
+        driver.switch_to.window(tab_main_id)
 
-delay_counter = 0
+        #Iterate through course links
+        courseRowValue = count_lines_in_file("SecondaryItinerary.txt") + 1
 
-while courseRowValue != 5005:
+        if count_lines_in_file("SecondaryItinerary.txt") < 2:
+            courseRowValue = 2
 
-    if delay_counter == 10:
-        sleep(random.randint(7, 15))
+        # Save course details within a txt file
+        # courseItinerary = []
+
         delay_counter = 0
 
-    random_delay_1 = random.randint(2, 6)
-    random_delay_2 = random.randint(5, 9)
+        while courseRowValue != 5005:
 
-    courseLink = f"/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/table[2]/tbody/tr[{courseRowValue}]/td[3]/a"
-    currentCourseURL = driver.find_element(By.XPATH, courseLink).get_attribute("href")
+            if delay_counter == 10:
+                sleep(random.randint(7, 15))
+                delay_counter = 0
 
-    sleep(random_delay_1)
+            random_delay_1 = random.randint(2, 3)
+            random_delay_2 = random.randint(2, 3)
 
-    driver.switch_to.window(tab_secondary_id)
-    driver.get(currentCourseURL)
+            courseLink = f"/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/table[2]/tbody/tr[{courseRowValue}]/td[3]/a"
+            currentCourseURL = driver.find_element(By.XPATH, courseLink).get_attribute("href")
 
-    course_title = driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/table[1]/tbody/tr/td[1]/h1").text
-    course_description = driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/p[3]").text
+            sleep(random_delay_1)
 
-    sleep(random_delay_2)
+            driver.switch_to.window(tab_secondary_id)
+            driver.get(currentCourseURL)
 
-    print(f"{course_title} | {course_description[:20]}")
+            course_title = driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/table[1]/tbody/tr/td[1]/h1").text
+            course_description = driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/p[3]").text
 
-    courseObject = {
-        "course_title": course_title,
-        "course_description": course_description
-    }
+            sleep(random_delay_2)
 
-    courseItinerary.append(courseObject)
+            print(f"{course_title} | {course_description[:20]}")
 
-    add_json_entry(courseObject, "SecondaryItinerary.txt")
+            courseObject = {
+                "course_title": course_title,
+                "course_description": course_description
+            }
 
-    driver.switch_to.window(tab_main_id)
-    courseRowValue += 1
-    delay_counter += 1
+            # courseItinerary.append(courseObject)
 
-with open('CourseItinerary.txt', 'w') as json_file:
-    json.dump(courseItinerary, json_file)
+            add_json_entry(courseObject, "SecondaryItinerary.txt")
+
+            driver.switch_to.window(tab_main_id)
+            courseRowValue += 1
+            delay_counter += 1
+        processing = False
+    except Exception as e: 
+        print(f"Error at Course Row Value: {courseRowValue}. Restarting...")
+        print(e)
+        sleep(15)
+
+# with open('CourseItinerary.txt', 'w') as json_file:
+#     json.dump(courseItinerary, json_file)
 
 
 
